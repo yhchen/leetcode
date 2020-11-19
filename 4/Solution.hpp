066653a636739c58d52cd73dee7038824221fde0
yhchen <myself559@gmail.com>
2020-11-19 15:18:52 +0800

    4

---
+++ 4/Solution.hpp
@@ -0,0 +1,12 @@
+//
+// Created by langrensha0016 on 2020/11/19.
+//
+
+#ifndef INC_4_SOLUTION_HPP
+#define INC_4_SOLUTION_HPP
+
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size){
+    double mid = 0;
+}
+
+#endif //INC_4_SOLUTION_HPP
