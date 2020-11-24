f4d8e1a0d8e90f812453fabcf7ff037e7cd57b56
yhchen <myself559@hotmail.com>
2020-11-24 12:07:30 +0800

    222 not completed

--- 222/Solution.hpp
+++ 222/Solution.hpp
@@ -5,4 +5,59 @@
 #ifndef INC_222_SOLUTION_HPP
 #define INC_222_SOLUTION_HPP
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+int calcDeepRight(struct TreeNode* root) {
+    int deep = 0;
+    while (root) {
+        ++deep;
+        root = root->right;
+    }
+    return deep;
+}
+
+int calcDeepLeft(struct TreeNode* root) {
+    int deep = 0;
+    while (root) {
+        ++deep;
+        root = root->left;
+    }
+    return deep;
+}
+
+int calcLastLayerCount(struct TreeNode* root, int leftDeep, int rightDeep) {
+    int leftNode = 0;
+    int rightNode = 1 << leftDeep - 1;
+    int mid = (leftNode + rightNode) / 2;
+    int mod = 1 << rightDeep;
+    while (leftNode < rightNode) {
+        struct TreeNode* node = root;
+        int tmp = mod;
+        while (tmp > 0) {
+            node = mid & tmp ? node->right: node->left;
+            tmp = (tmp >> 1);
+        }
+        if (node) leftNode = mid;
+        else rightNode = mid;
+        mid = (leftNode + rightNode) / 2;
+    }
+    return (2 << leftDeep) - rightNode;
+}
+
+int countNodes(struct TreeNode* root) {
+    int leftDeep = calcDeepLeft(root);
+    int rightDeep = calcDeepRight(root);
+    if (leftDeep == rightDeep) {
+        return (2 << leftDeep) - 1;
+    }
+    return calcLastLayerCount(root, leftDeep, rightDeep);
+}
+
 #endif //INC_222_SOLUTION_HPP
---
+++ 222/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+struct TreeNode {
+    TreeNode *left;
+    TreeNode *right;
+};
+
+#include "Solution.hpp"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
