b2d16162b89ccc6386cbbb3d4df18bef1c4d2575
yhchen <myself559@hotmail.com>
2020-12-01 12:44:19 +0800

    976 completedls

---
+++ 767/Solution.hpp
@@ -0,0 +1,8 @@
+//
+// Created by mysel on 11/30/2020.
+//
+
+#ifndef INC_767_SOLUTION_HPP
+#define INC_767_SOLUTION_HPP
+
+#endif //INC_767_SOLUTION_HPP
--- 976/Solution.hpp
+++ 976/Solution.hpp
@@ -5,4 +5,19 @@
 #ifndef INC_976_SOLUTION_HPP
 #define INC_976_SOLUTION_HPP
 
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+    return *(const int*)a - *(const int*)b;
+}
+
+int largestPerimeter(int* A, int ASize){
+    if (A == 0 || ASize < 3) return 0;
+    qsort(A, ASize, sizeof(A[0]), &compare);
+    for (int i = ASize - 3; i > -1; --i) {
+        if (A[i] +A[i+1] >A[i+2]) return A[i] +A[i+1] +A[i+2];
+    }
+    return 0;
+}
+
 #endif //INC_976_SOLUTION_HPP
