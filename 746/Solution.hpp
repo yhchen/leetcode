fe0a4d77b3020fe62f21aa54c9241b5100966b51
yhchen <myself559@gmail.com>
2020-12-21 17:52:20 +0800

    746 complete

--- 746/Solution.hpp
+++ 746/Solution.hpp
@@ -5,8 +5,19 @@
 #ifndef INC_746_SOLUTION_HPP
 #define INC_746_SOLUTION_HPP
 
-int minCostClimbingStairs(int* cost, int costSize){
-    return 0;
+#define min(a, b) ((a) > (b) ? (b): (a))
+
+int minCostClimbingStairs(int *cost, int costSize) {
+    if (costSize <= 0) return 0;
+    else if (costSize == 1) return cost[0];
+    int a = cost[0], b = cost[1];
+    for (int i = 2; i < costSize; ++i) {
+        int tmp = a;
+        a = b;
+        b = min(tmp, b) + cost[i];
+    }
+
+    return min(a, b);
 }
 
 #endif //INC_746_SOLUTION_HPP
