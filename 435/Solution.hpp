99b3daccb8357d3b1f1f1f961bbf3e26589faee1
yhchen <myself559@gmail.com>
2020-12-31 17:41:44 +0800

    435 completed

--- 435/Solution.hpp
+++ 435/Solution.hpp
@@ -14,28 +14,19 @@ public:
     int eraseOverlapIntervals(vector<vector<int>> &intervals) {
         if (intervals.size() <= 1) return 0;
         std::sort(intervals.begin(), intervals.end(), [](const vector<int> &a, const vector<int> &b) -> bool {
-            if (a[0] != b[0]) return a[0] - b[0];
-            return a[1] - b[1];
+            return a[1] < b[1];
         });
-        for (auto vve: intervals) {
-            std::cout << vve[0] << ", " << vve[1] << std::endl;
-        }
-        vector<int>* vv = &intervals[0];
-        int result =0;
-        const int size = (int)intervals.size();
+        int end = intervals[0][1];
+        int result = 0;
+        const int size = (int) intervals.size();
         for (int i = 1; i < size; ++i) {
-            if (!inRange(*vv, intervals[i])) {
-                vv = &intervals[i];
-                break;
+            if (intervals[i][0] >= end) {
+                end = intervals[i][1];
+//                std::cout << "end: " << end << std::endl;
             }
-            ++result;
+            else ++result;
         }
         return result;
-    }
-
-private:
-    bool inRange(const vector<int>& a, const vector<int>& b) {
-        return a[0] < b[1] && a[1] > b[0];
     }
 };
 
--- 435/main.cpp
+++ 435/main.cpp
@@ -1,13 +1,43 @@
 #include <iostream>
 #include "Solution.hpp"
 
-int main() {
+int test1() {
     Solution s;
     vector<vector<int>> vv;
+
     vv.push_back(std::vector<int>({1, 2}));
     vv.push_back(std::vector<int>({2, 3}));
     vv.push_back(std::vector<int>({3, 4}));
     vv.push_back(std::vector<int>({1, 3}));
-    auto result = s.eraseOverlapIntervals(vv);
+
+    std::cout << s.eraseOverlapIntervals(vv) << std::endl;
+}
+
+int test2() {
+    Solution s;
+    vector<vector<int>> vv;
+
+    vv.push_back(std::vector<int>({1, 2}));
+    vv.push_back(std::vector<int>({2, 4}));
+    vv.push_back(std::vector<int>({3, 4}));
+
+    std::cout << s.eraseOverlapIntervals(vv) << std::endl;
+}
+
+int test3() {
+    Solution s;
+    vector<vector<int>> vv;
+
+    vv.push_back(std::vector<int>({1, 2}));
+    vv.push_back(std::vector<int>({11, 12}));
+    vv.push_back(std::vector<int>({22, 33}));
+
+    std::cout << s.eraseOverlapIntervals(vv) << std::endl;
+}
+
+int main() {
+    test1();
+    test2();
+    test3();
     return 0;
 }
