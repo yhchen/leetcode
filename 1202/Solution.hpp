96a1a4ef0c8fb48fb3c407c447f163329e71e5e2
yhchen <myself559@gmail.com>
2021-01-12 11:17:59 +0800

    1202 not completed

--- 1202/Solution.hpp
+++ 1202/Solution.hpp
@@ -25,7 +25,7 @@ using namespace std;
 class Solution {
 public:
     string smallestStringWithSwaps(string s, vector<vector<int>> &pairs) {
-        this->makeNewMap(pairs);
+        this->makeNewMap(pairs, s);
         // sort groups
         for (auto&[k, rV]: this->_mvGroupIndexes) {
             vector<char> tmpVec;
@@ -51,47 +51,46 @@ public:
     }
 
 private:
-    void makeNewMap(const vector<vector<int>> &pairs) {
+    void makeNewMap(const vector<vector<int>> &pairs, const string &s) {
         // clear
         this->_mvGroupIndexes.clear();
-        this->_mIndexGroup.clear();
-        this->_nGroup = 0;
+        int *pIndexGroup = new int[s.size() + 1]{0};
+        vector<int> vGroup;
+
         // make indexes
         for (const auto &vc: pairs) {
             int i1 = vc[0], i2 = vc[1];
             if (i1 == i2) continue;
-            int g1 = this->_mIndexGroup[i1], g2 = this->_mIndexGroup[i2];
+            int g1 = vGroup[pIndexGroup[i1]], g2 = vGroup[pIndexGroup[i2]];
             if (g1 != 0 && g2 != 0) {
                 if (g1 != g2) {
                     // merge g2 -> g1
                     auto &rvg1 = this->_mvGroupIndexes[g1];
                     auto &rvg2 = this->_mvGroupIndexes[g2];
-                    for (int idx: rvg2) {
-                        this->_mIndexGroup[idx] = g1;
-                        rvg1.push_back(idx);
-                    }
+                    rvg1.assign(rvg2.begin(), rvg2.end());
+                    vGroup[g2] = g1;
                     this->_mvGroupIndexes.erase(g2);
                 }
             } else if (g1 != 0 || g2 != 0) {
                 // add i1(or i2) to g2(or g1)
                 if (g1 != 0) {
-                    this->_mIndexGroup[i2] = g1;
+                    pIndexGroup[i2] = g1;
                     this->_mvGroupIndexes[g1].push_back(i2);
                 } else {
-                    this->_mIndexGroup[i1] = g2;
+                    pIndexGroup[i1] = g2;
                     this->_mvGroupIndexes[g2].push_back(i1);
                 }
             } else {
                 // add new group
-                int newGroup = ++this->_nGroup;
-                this->_mIndexGroup[i1] = this->_mIndexGroup[i2] = newGroup;
+                vGroup.push_back(vGroup.size() + 1);
+                int newGroup = vGroup.size();
+                pIndexGroup[i1] = pIndexGroup[i2] = newGroup;
                 this->_mvGroupIndexes.insert(make_pair(newGroup, std::vector<int>({i1, i2})));
             }
         }
+        delete[]pIndexGroup;
     }
 
-    unordered_map<int, int> _mIndexGroup;
-    int _nGroup = 0;
     unordered_map<int, vector<int>> _mvGroupIndexes;
 };
 
