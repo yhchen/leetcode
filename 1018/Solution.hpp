896e7f3bd6bc2a425f4a580286557fbfb6a3fc67
yhchen <myself559@gmail.com>
2021-01-14 17:18:54 +0800

    1018 second result

--- 1018/Solution.hpp
+++ 1018/Solution.hpp
@@ -9,18 +9,39 @@
 
 using namespace std;
 
+//class Solution {
+//public:
+//    vector<bool> prefixesDivBy5(vector<int>& A) {
+//        vector<bool> result;
+//        int tmpval = 0;
+//        for (const auto& v: A) {
+//            tmpval = ((tmpval << 1) + v) % 5;
+//            result.push_back(tmpval == 0);
+//        }
+//        return result;
+//    }
+//};
+
+
 class Solution {
 public:
-    vector<bool> prefixesDivBy5(vector<int>& A) {
-        vector<bool> result;
-        int tmpval = 0;
-        for (const auto& v: A) {
-            tmpval = ((tmpval << 1) + v) % 5;
-            result.push_back(tmpval == 0);
+    vector<bool> prefixesDivBy5(std::vector<int> A) {
+        vector<bool> res;
+        if (A.size() < 1) return res;
+        const static int StateSet[5][2] = {
+                {0, 1},
+                {2, 3},
+                {4, 0},
+                {1, 2},
+                {3, 4}
+        };
+        int state = 0;
+        for (int i = 0; i < A.size(); i++) {
+            state = StateSet[state][A[i]];  //转换后的状态
+            res.push_back(state == 0);
         }
-        return result;
+        return res;
     }
 };
-
 
 #endif //INC_1018_SOLUTION_HPP
