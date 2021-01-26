6f4fcfb6266ff6d16c629417634a30d6742dd1d9
yhchen <myself559@gmail.com>
2021-01-26 17:33:06 +0800

    947 complete

--- 947/Solution.hpp
+++ 947/Solution.hpp
@@ -6,47 +6,59 @@
 #define INC_947_SOLUTION_HPP
 
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
-class Solution {
-private:
-    int n = 20005; // 根据坐标范围而定
-    int father[20005];
+// https://leetcode-cn.com/problems/most-stones-removed-with-same-row-or-column/solution/tu-jie-bing-cha-ji-by-yexiso-nbcz/
 
-    // 并查集初始化
-    void init() {
-        for (int i = 0; i < n; ++i) {
-            father[i] = i;
-        }
-    }
-    // 并查集里寻根的过程
-    int find(int u) {
-        return u == father[u] ? u : father[u] = find(father[u]);
-    }
-    // 将v->u 这条边加入并查集
-    void join(int u, int v) {
-        u = find(u);
-        v = find(v);
-        if (u == v) return ;
-        father[v] = u;
-    }
-    // 判断 u 和 v是否找到同一个根
-    bool same(int u, int v) {
-        u = find(u);
-        v = find(v);
-        return u == v;
-    }
+class Djset {
 public:
-    int removeStones(vector<vector<int>>& stones) {
-        init();
-        for (int i = 0; i < stones.size(); i++) join(stones[i][0], stones[i][1] + 10000);
-        unordered_map<int, bool> umap;
-        for (int i = 0; i < stones.size(); i++) {
-            umap[find(stones[i][0])] = true;
-            umap[find(stones[i][1] + 10000)] = true;
+    unordered_map<int, int> parent, rank;  // 记录节点的根
+    int count;
+
+    Djset(int n) : count(0) {}
+
+    int find(int x) {
+        // 添加了一个新的集合，count+1
+        if (parent.find(x) == parent.end()) {
+            parent[x] = x;
+            count++;
         }
-        return stones.size() - umap.size();
+        // 压缩方式：直接指向根节点
+        if (x != parent[x]) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    void merge(int x, int y) {
+        int rootx = find(x);
+        int rooty = find(y);
+        if (rootx != rooty) {
+            if (rank[rootx] < rank[rooty]) {
+                swap(rootx, rooty);
+            }
+            parent[rooty] = rootx;
+            if (rank[rootx] == rank[rooty]) rank[rootx] += 1;
+            count--;
+        }
+    }
+
+    int get_count() {
+        return count;
+    }
+};
+
+class Solution {
+public:
+    int removeStones(vector<vector<int>> &stones) {
+        int n = stones.size();
+        Djset ds(n);
+        for (auto &e : stones) {
+            ds.merge(e[0] + 10001, e[1]);
+        }
+        return n - ds.get_count();
     }
 };
 
