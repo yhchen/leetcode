0d8c39b9f79a6c5f79a0a4def8c2333f59f8c4d5
yhchen <myself559@gmail.com>
2021-03-22 17:04:37 +0800

    331 completed

---
+++ 331/Solution.hpp
@@ -0,0 +1,26 @@
+#ifndef __SOLUTION_H__
+#define __SOLUTION_H__
+
+#include <string>
+#include <vector>
+
+class Solution {
+public:
+    bool isValidSerialization(std::string preorder) {
+        size_t sz = preorder.size();
+        const char *data = preorder.c_str();
+        int need_scope = 1;
+        printf("-----------------------------\n");
+        for (size_t i = 0; i < sz; ++i) {
+            if (need_scope <= 0) return false;
+            if (data[i] == '#') --need_scope;
+            else ++need_scope;
+            // printf("c: \"%c\" scope: %d\n", data[i], need_scope);
+            // find next ','
+            while (i < sz && data[++i] != ',');
+        }
+        return need_scope == 0;
+    }
+};
+
+#endif // __SOLUTION_H__
---
+++ 331/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Solution.hpp"
+
+int main() {
+    Solution solution;
+    std::cout << solution.isValidSerialization("1,2,3,#,#,#") << std::endl;
+    return 0;
+}
