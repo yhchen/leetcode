a6bc05ca6212a6f918e499c713ca43b2c32a3ee1
yhchen <myself559@gmail.com>
2021-03-23 15:09:41 +0800

    191 completed

---
+++ 191/Solution.hpp
@@ -0,0 +1,20 @@
+//
+// Created by yhchen on 2021/3/22.
+//
+
+#ifndef INC_191_SOLUTION_HPP
+#define INC_191_SOLUTION_HPP
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        n = (n & 0x55555555) + ((n >> 1) & 0x55555555);
+        n = (n & 0x33333333) + ((n >> 2) & 0x33333333);
+        n = (n & 0x0f0f0f0f) + ((n >> 4) & 0x0f0f0f0f);
+        n = (n & 0x00ff00ff) + ((n >> 8) & 0x00ff00ff);
+        n = (n & 0x0000ffff) + ((n >> 16) & 0x0000ffff);
+        return n;
+    }
+};
+
+#endif //INC_191_SOLUTION_HPP
