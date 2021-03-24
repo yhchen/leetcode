a8edf128ac8b9ebc58ba5b053141200ad0dcfdaf
yhchen <myself559@gmail.com>
2021-03-24 10:03:36 +0800

    1.增加不需要额外申请大块内存的方式

--- 341/Solution.h
+++ 341/Solution.h
@@ -26,29 +26,102 @@
 class NestedIterator {
 public:
     NestedIterator(std::vector<NestedInteger> &nestedList) {
-        dfs(nestedList);
-    }
-
-    int next() {
-        return _datas[_index++];
-    }
-
-    bool hasNext() {
-        return _index < _datas.size();
-    }
-
-private:
-    void dfs(const std::vector<NestedInteger> &nestedList) {
-        for (int i = 0; i < nestedList.size(); ++i) {
-            auto &integer = nestedList[i];
-            if (integer.isInteger()) _datas.push_back(integer.getInteger());
-            else dfs(integer.getList());
+        if (nestedList.size() > 0) {
+            _stacks.push_back({&nestedList, 0});
+            tryGetNext();
         }
     }
 
-    std::vector<int> _datas;
-    size_t _index = 0;
+    int next() {
+        auto _tmp = _next;
+        tryGetNext();
+        return _tmp;
+    }
+
+    bool hasNext() {
+        return _hasNext;
+    }
+
+private:
+    struct NestedStacks {
+        const std::vector<NestedInteger> *list;
+        int index;
+
+        inline bool end() const { return index >= list->size(); }
+
+        inline const NestedInteger &getNext() { return (*list)[index++]; }
+    };
+
+    void tryGetNext() {
+        while (_stacks.size() > 0) {
+            auto &stack = _stacks[_stacks.size() - 1];
+            const auto &pdata = stack.getNext();
+            if (!pdata.isInteger()) {
+                clearStack();
+                if (pdata.getList().size() > 0) {
+                    _stacks.push_back({&pdata.getList(), 0});
+                }
+                continue;
+            }
+            // found & clear
+            clearStack();
+            _next = pdata.getInteger();
+            _hasNext = true;
+            return;
+        }
+        _next = 0;
+        _hasNext = false;
+    }
+
+    void clearStack() {
+        while (_stacks.size() > 0) {
+            auto &stack = _stacks[_stacks.size() - 1];
+            if (stack.end()) {
+                _stacks.pop_back();
+                continue;
+            }
+            break;
+        }
+    }
+
+    std::vector<NestedStacks> _stacks;
+    int _next;
+    bool _hasNext;
 };
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+//class NestedIterator {
+//public:
+//    NestedIterator(vector<NestedInteger> &nestedList)
+//    {
+//        dfs(nestedList);
+//    }
+//
+//    int next() {
+//        return _datas[_index++];
+//    }
+//
+//    bool hasNext() {
+//        return _index < _datas.size();
+//    }
+//
+//private:
+//    void dfs(vector<NestedInteger> &nestedList)
+//    {
+//        for (int i = 0 ;i < nestedList.size(); ++i) {
+//            auto& integer = nestedList[i];
+//            if (integer.isInteger()) _datas.push_back(integer.getInteger());
+//            else dfs(integer.getList());
+//        }
+//    }
+//
+//    vector<int> _datas;
+//    size_t _index = 0;
+//};
+
+////////////////////////////////////////////////////////////////////////////////
 
 /**
  * Your NestedIterator object will be instantiated and called as such:
