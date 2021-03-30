5e0ff946fb09682f20c171a92535ba9c84c1e0c2
yhchen <myself559@gmail.com>
2021-03-30 17:33:10 +0800

    74 not completed

---
+++ 74/Solution.hpp
@@ -0,0 +1,8 @@
+//
+// Created by langrensha0016 on 2021/3/30.
+//
+
+#ifndef INC_74_SOLUTION_HPP
+#define INC_74_SOLUTION_HPP
+
+#endif //INC_74_SOLUTION_HPP
---
+++ 74/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+
+#include "Solution.hpp"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
