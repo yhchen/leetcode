c484ec468ee53c64ed4317b64faa91ba0fdfb7f6
yhchen <myself559@gmail.com>
2021-03-31 15:05:58 +0800

    90 completed

--- 90/Solution.hpp
+++ 90/Solution.hpp
@@ -5,4 +5,43 @@
 #ifndef INC_90_SOLUTION_HPP
 #define INC_90_SOLUTION_HPP
 
+class Solution {
+public:
+    std::vector <std::vector<int>> subsetsWithDup(std::vector<int> &nums) {
+        std::vector <std::vector<int>> sub;
+        std::unordered_map<int, int> sub_map;
+        std::vector <std::pair<int, int>> vecGroups;
+        for (auto v: nums) {
+            ++sub_map[v];
+        }
+        for (auto iter: sub_map) {
+            vecGroups.push_back(iter);
+        }
+
+        makeSub(sub, vecGroups, 0);
+
+        return sub;
+    }
+
+private:
+    void makeSub(std::vector <std::vector<int>> &sub, std::vector <std::pair<int, int>> &vecGroups, int index) {
+        if (index >= vecGroups.size()) {
+            sub.push_back(_data);
+            return;
+        }
+        auto &kv = vecGroups[index];
+        int len = _data.size();
+        makeSub(sub, vecGroups, index + 1);
+        _data.resize(len);
+        for (int i = kv.second; i > 0; --i) {
+            _data.push_back(kv.first);
+            int len = _data.size();
+            makeSub(sub, vecGroups, index + 1);
+            _data.resize(len);
+        }
+    }
+
+    std::vector<int> _data;
+};
+
 #endif //INC_90_SOLUTION_HPP
--- 90/main.cpp
+++ 90/main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+
+#include "Solution.hpp"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
