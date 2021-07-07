e813c31a05b3f06d7870c080005e101713101790
manyu <manyu@zmhy.com>
2021-07-07 19:36:56 +0800

    1711 not completed

---
+++ 1711/Solution.hpp
@@ -0,0 +1,11 @@
+#ifndef __SOLUTION_HPP__
+#define __SOLUTION_HPP__
+
+class Solution {
+public:
+    int countPairs(vector<int> &deliciousness) {
+        return 0;
+    }
+};
+
+#endif // __SOLUTION_HPP__
