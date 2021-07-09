6e8e532dc68cb6d30685f904beb677c8f4b2e2fa
manyu <manyu@zmhy.com>
2021-07-09 16:54:49 +0800

    930 test case

--- 930/main.cpp
+++ 930/main.cpp
@@ -4,11 +4,11 @@
 
 int main() {
     Solution solution;
-//    std::cout << solution.numSubarraysWithSum(std::vector<int>{1,0,1,0,1}, 0) << std::endl;
-//    std::cout << solution.numSubarraysWithSum(std::vector<int>{0,1,0,1,0}, 0) << std::endl;
-//    std::cout << solution.numSubarraysWithSum(std::vector<int>{0,1,0,0,0,0}, 0) << std::endl;
-//    std::cout << solution.numSubarraysWithSum(std::vector<int>{0,0,0,0,0,0}, 0) << std::endl;
-//    std::cout << solution.numSubarraysWithSum(std::vector<int>{1,1,1,1,1,1}, 0) << std::endl;
+    std::cout << solution.numSubarraysWithSum(std::vector<int>{1,0,1,0,1}, 0) << std::endl;
+    std::cout << solution.numSubarraysWithSum(std::vector<int>{0,1,0,1,0}, 0) << std::endl;
+    std::cout << solution.numSubarraysWithSum(std::vector<int>{0,1,0,0,0,0}, 0) << std::endl;
+    std::cout << solution.numSubarraysWithSum(std::vector<int>{0,0,0,0,0,0}, 0) << std::endl;
+    std::cout << solution.numSubarraysWithSum(std::vector<int>{1,1,1,1,1,1}, 0) << std::endl;
 
     std::cout << solution.numSubarraysWithSum(std::vector<int>{1,0,1,0,1,0,1,0,1,0,1}, 1) << std::endl;
     std::cout << solution.numSubarraysWithSum(std::vector<int>{1,0,1,0,1,0,1,0,1,0,1}, 2) << std::endl;
