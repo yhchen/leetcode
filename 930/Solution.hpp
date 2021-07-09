26e5cbbbf4c6ccd1eed1c8d38d834cf76b915110
manyu <manyu@zmhy.com>
2021-07-09 16:59:48 +0800

    优化 goal = 0 部分的逻辑（可与其他逻辑合并）

--- 930/Solution.hpp
+++ 930/Solution.hpp
@@ -11,46 +11,39 @@ class Solution {
 public:
     int numSubarraysWithSum(const std::vector<int> &nums, int goal) {
         if (goal > nums.size()) return 0;
-        {
-            int one = 0;
-            for (auto &num: nums) {
-                if (num) ++one;
-            }
-            if (goal > one) return 0;
-        }
 
         int result = 0;
-        if (goal == 0) {
-            int index = 0;
-            while (nums[index++] && index < nums.size());
-            --index;
-            int zero = 0;
-            result = zero;
-            while (index < nums.size()) {
-                if (nums[index++]) zero = 0;
-                else result += ++zero;
+//        if (goal == 0) {
+//            int index = 0;
+//            while (nums[index++] && index < nums.size());
+//            --index;
+//            int zero = 0;
+//            result = zero;
+//            while (index < nums.size()) {
+//                if (nums[index++]) zero = 0;
+//                else result += ++zero;
+//            }
+//            return result;
+//        }
+
+        const int queue_size = goal + 1; //
+        int *p_queue = new int[queue_size]{0};
+        int queue_idx = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i]) { // is one
+                ++p_queue[queue_idx];
+                queue_idx = (queue_idx + 1) % queue_size;
+                p_queue[queue_idx] = 0;
+            } else { // is zero
+                ++p_queue[queue_idx];
             }
-        } else {
-            const int queue_size = goal + 1; //
-            int *p_queue = new int[queue_size]{0};
-            int queue_idx = 0;
-            for (int i = 0; i < nums.size(); ++i) {
-                if (nums[i]) { // is one
-                    ++p_queue[queue_idx];
-                    queue_idx = (queue_idx + 1) % queue_size;
-                    p_queue[queue_idx] = 0;
-                } else { // is zero
-                    ++p_queue[queue_idx];
-                }
-                int last_index = (queue_size + queue_idx - goal) % queue_size;
-                result += p_queue[last_index];
+            int last_index = (queue_size + queue_idx - goal) % queue_size;
+            result += p_queue[last_index];
 //                std::cout << nums[i] << ":[";
 //                for (int j = 0; j < queue_size; ++j) std::cout << (j == 0 ? "" : ",") << p_queue[j];
 //                std::cout << "]" << std::endl;
-            }
-            delete[] p_queue;
         }
-
+        delete[] p_queue;
 
         return result;
     }
